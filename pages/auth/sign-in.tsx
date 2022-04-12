c1061b1e5cd4249e8a83e88900e66a875c8b4ae9
Ashik Rahman <ashrhmn99@gmail.com>
2022-04-12 09:05:18 +0600

    Add styles to signin page

--- pages/auth/sign-in.tsx
+++ pages/auth/sign-in.tsx
@@ -4,6 +4,7 @@ import useInput from "../../hooks/useInput";
 import { service } from "../../service";
 import Cookies from "js-cookie";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 const SignInPage: NextPage = () => {
   const usernameInputController = useInput();
@@ -22,10 +23,34 @@ const SignInPage: NextPage = () => {
     }
   };
   return (
-    <div>
-      <input type="text" {...usernameInputController} />
-      <input type="password" {...passwordInputController} />
-      <button onClick={handleLogin}>Sign In</button>
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className="flex p-6 border-2 rounded-lg flex-col w-96 justify-center gap-10 items-center text-2xl">
+        <input
+          className="w-full p-4 rounded"
+          placeholder="Username"
+          type="text"
+          {...usernameInputController}
+        />
+        <input
+          className="w-full p-4 rounded"
+          placeholder="Password"
+          type="password"
+          {...passwordInputController}
+        />
+        <div className="flex gap-4 justify-center">
+          <button
+            className="bg-green-500 text-white hover:bg-green-700 transition-colors p-4 rounded w-40"
+            onClick={handleLogin}
+          >
+            Sign In
+          </button>
+          <Link passHref href={`/auth/sign-up`}>
+            <button className="bg-blue-500 text-white hover:bg-blue-700 transition-colors p-4 rounded w-40">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
