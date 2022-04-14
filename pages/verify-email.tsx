7d2d33613604034bddda01a899c7c1f198071bb5
Ashik Rahman <ashrhmn99@gmail.com>
2022-04-15 03:06:25 +0600

    fix ux

--- pages/verify-email.tsx
+++ pages/verify-email.tsx
@@ -1,11 +1,19 @@
 import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import Cookies from "js-cookie";
-import React from "react";
+import React, { useState } from "react";
 import { baseApiUrl } from "../consts";
 import { service } from "../service";
+import { useRouter } from "next/router";
 
 const VerifyEmailPage: NextPage = () => {
+  const router = useRouter();
+  const [resendBtnDisabled, setResendBtnDisabled] = useState(false);
+  const handleLogout = () => {
+    Cookies.remove("token");
+    router.reload();
+  };
   const handleResendMail = async () => {
+    setResendBtnDisabled(() => true);
     try {
       const token = Cookies.get("token");
       const { data: response } = await service().get(
@@ -17,6 +25,7 @@ const VerifyEmailPage: NextPage = () => {
       console.log(error);
       alert("Error sending mail");
     }
+    setResendBtnDisabled(false);
   };
   return (
     <div className="flex h-[100vh] flex-col justify-center items-center text-2xl text-center">
@@ -24,10 +33,17 @@ const VerifyEmailPage: NextPage = () => {
         Please verify your email. If you have already try refreshing the page
       </h1>
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white transition-colors rounded p-2 w-80"
+        className="bg-blue-500 disabled:bg-blue-300 disabled:cursor-progress hover:bg-blue-700 text-white transition-colors rounded p-2 w-80 m-2"
+        disabled={resendBtnDisabled}
         onClick={handleResendMail}
       >
         Re-send verification mail
+      </button>
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white transition-colors rounded p-2 w-80 m-2"
+        onClick={handleLogout}
+      >
+        Logout
       </button>
     </div>
   );
